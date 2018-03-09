26e40f1b623bd32197a466104b3e24ac69b20a18
daniel kelly <danieljkelly91@gmail.com>
2018-03-09 12:28:43 +0000

    adds getters for make, model, colour to instrument class and  type, numberOfStrings in guitar class

--- src/main/java/Guitar.java
+++ src/main/java/Guitar.java
@@ -1,8 +1,12 @@
 public class Guitar extends Instrument {
+    private GuitarType type;
+    private int numberOfStrings;
 
 
     public Guitar(GuitarType type, int numberOfStrings, String make, String model, String colour, String material, String sound, double tradePrice, double retailPrice) {
         super(make, model, colour, material, sound, tradePrice, retailPrice);
+        this.type = type;
+        this.numberOfStrings = numberOfStrings;
     }
 
     @Override
@@ -13,5 +17,13 @@ public class Guitar extends Instrument {
     @Override
     public double calculateMarkUp() {
         return 0;
+    }
+
+    public GuitarType getType() {
+        return type;
+    }
+
+    public int getNumberOfStrings() {
+        return numberOfStrings;
     }
 }
--- src/main/java/Instrument.java
+++ src/main/java/Instrument.java
@@ -15,4 +15,24 @@ public abstract class Instrument extends Product implements Playable{
         this.material = material;
         this.sound = sound;
     }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getSound() {
+        return sound;
+    }
 }
