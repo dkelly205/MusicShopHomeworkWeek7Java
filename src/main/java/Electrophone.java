4802b2664c994f7c9bbb018a098634e335140b89
daniel kelly <danieljkelly91@gmail.com>
2018-03-09 14:34:10 +0000

    added getters for numberOfKeys and electrophoneType

--- src/main/java/Electrophone.java
+++ src/main/java/Electrophone.java
@@ -1,8 +1,14 @@
+import java.lang.annotation.ElementType;
+
 public class Electrophone extends Instrument {
 
+    private ElectroPhoneType type;
+    private int numberOfKeys;
 
-    public Electrophone(KeyboardType, String make, String model, String colour, String material, String sound, double tradePrice, double retailPrice) {
+    public Electrophone(ElectroPhoneType type, int numberOfKeys, String make, String model, String colour, String material, String sound, double tradePrice, double retailPrice) {
         super(make, model, colour, material, sound, tradePrice, retailPrice);
+        this.type = type;
+        this.numberOfKeys = numberOfKeys;
 
     }
 
@@ -14,5 +20,13 @@ public class Electrophone extends Instrument {
     @Override
     public double calculateMarkUp() {
         return 0;
+    }
+
+    public ElectroPhoneType getType() {
+        return type;
+    }
+
+    public int getNumberOfKeys() {
+        return numberOfKeys;
     }
 }
--- src/test/java/ElectrophoneTest.java
+++ src/test/java/ElectrophoneTest.java
@@ -1,14 +1,25 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ElectrophoneTest {
+
+    Electrophone keyboard;
 
     @Before
     public void setUp() throws Exception {
+        keyboard = new Electrophone(ElectroPhoneType.ELECTRONIC_KEYBOARD, 24, "Casio", "500", "black/white", "plastic", "beep", 20.00, 50.00);
         
     }
 
     @Test
-    public void name() {
+    public void testType() {
+        assertEquals(ElectroPhoneType.ELECTRONIC_KEYBOARD, keyboard.getType());
+    }
+
+    @Test
+    public void testNumberOfKeys() {
+        assertEquals(24, keyboard.getNumberOfKeys());
     }
 }
